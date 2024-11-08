9944b6c165904afcb1b2e2365489d00e9bc32648
Tadashi Saito <tad.a.digger@gmail.com>
2024-11-08 22:30:55 +0900

    Implement Epsilon GC with Bump allocation.

--- intern.h
+++ intern.h
@@ -59,6 +59,8 @@ typedef struct {
     Value retval;
 } Continuation;
 
+#define VALUE_TAG(v) (*(ValueTag *)(v))
+
 #define PAIR(v) ((Pair *) v)
 #define LOCATED_PAIR(v) ((LocatedPair *) v)
 #define STRING(v) ((String *) v)
@@ -74,6 +76,11 @@ ATTR_HIDDEN void pos_to_line_col(int64_t pos, Value newline_pos, int64_t *line, 
 ATTR_HIDDEN ATTR(noreturn) void raise_error(jmp_buf buf, const char *fmt, ...);
 ATTR_HIDDEN Value reverse(Value l);
 ATTR_HIDDEN void *obj_new(size_t size, ValueTag t);
+
+ATTR_HIDDEN void gc_init(void);
+ATTR_HIDDEN void gc_stack_init(const volatile void *b);
+ATTR_HIDDEN size_t gc_stack_get_size(const volatile void *sp);
+ATTR_HIDDEN ATTR_XMALLOC void *gc_malloc(size_t size);
 
 static inline Value list1(Value x)
 {
