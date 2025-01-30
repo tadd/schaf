tadd/my-c-lisp

Split table.[ch].

Makefile
@@ -5,7 +5,7 @@ LIBS=-lm
 ANALYZER=-fanalyzer
 SANITIZER=-fsanitize=undefined #,address
 
-OBJ_COMMON=lisp.o utils.o scary.o
+OBJ_COMMON=lisp.o utils.o scary.o table.o
 OBJ=$(OBJ_COMMON) main.o
 OBJ_TEST=$(OBJ_COMMON) basic-test.o
 
@@ -58,9 +58,12 @@ basic-test-san: $(OBJ_TEST:.o=.san.o)
 microbench:
 	@$(MAKE) -C $@
 
+scary.o: scary.h
 utils.o: utils.h
-lisp.o scary.o: scary.h
-lisp.o main.o basic-test.o: lisp.h utils.h
+main.o: lisp.h utils.h
+table.o: table.h utils.h
+basic-test.o: lisp.h utils.h table.h
+lisp.o: lisp.h utils.h scary.h table.h
 
 .PHONY: all clean test test-c test-scheme analyze sanitize \
 	test-san test-c-san test-scheme-san \

tadd/schaf

Makefile
@@ -6,7 +6,7 @@ ANALYZER=-fanalyzer
 SANITIZER=-fsanitize=undefined #,address
 TIMEOUT=timeout 2
 
-OBJ_COMMON=schaf.o utils.o scary.o
+OBJ_COMMON=schaf.o utils.o scary.o table.o
 OBJ=$(OBJ_COMMON) main.o
 OBJ_TEST=$(OBJ_COMMON) basic-test.o
 
@@ -59,9 +59,12 @@ basic-test-san: $(OBJ_TEST:.o=.san.o)
 microbench:
 	@$(MAKE) -C $@
 
+scary.o: scary.h
 utils.o: utils.h
 schaf.o main.o basic-test.o: schaf.h utils.h
 schaf.o scary.o: scary.h
+table.o: table.h utils.h
+schaf.o basic-test.o: table.h
 
 .PHONY: all clean test test-c test-scheme analyze sanitize \
 	test-san test-c-san test-scheme-san \