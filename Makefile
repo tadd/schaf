tadd/my-c-lisp

Fix and cleanse file dependency.

Makefile
@@ -56,9 +56,8 @@ basic-test-san: $(OBJ_TEST:.o=.san.o)
 	$(CC) $(CFLAGS) $(SANITIZER) -c $< -o $@
 
 utils.o: utils.h
-lisp.o main.o: lisp.h utils.h
-lisp.o: scary.h
-basic-test.o: lisp.h
+lisp.o scary.o: scary.h
+lisp.o main.o basic-test.o: lisp.h utils.h
 
 .PHONY: all clean test test-c test-scheme analyze sanitize \
 	test-san test-c-san test-scheme-san

tadd/schaf

Makefile
@@ -60,8 +60,8 @@ microbench:
 	@$(MAKE) -C $@
 
 utils.o: utils.h
-schaf.o main.o: schaf.h utils.h
-basic-test.o: schaf.h
+schaf.o main.o basic-test.o: schaf.h utils.h
+schaf.o scary.o: scary.h
 
 .PHONY: all clean test test-c test-scheme analyze sanitize \
 	test-san test-c-san test-scheme-san \