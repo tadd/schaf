63669cf3d7dbe41eb2755923ed5a3e68902fc2d8
Tadashi Saito <tad.a.digger@gmail.com>
2025-04-10 23:58:58 +0900

    Fix comments.

--- utils.h
+++ utils.h
@@ -25,8 +25,8 @@ extern const uint64_t TABLE_NOT_FOUND;
 Table *table_new(void);
 Table *table_inherit(const Table *t);
 void table_free(Table *t);
-Table *table_put(Table *t, uint64_t key, uint64_t val); // `val` can't be 0
-bool table_set(Table *t, uint64_t key, uint64_t val); // set (only) if found
+Table *table_put(Table *t, uint64_t key, uint64_t val); // `val` can't be TABLE_NOT_FOUND
+bool table_set(Table *t, uint64_t key, uint64_t val); // set if found
 uint64_t table_get(const Table *t, uint64_t key);
 void table_foreach(const Table *t, TableForeachFunc f, void *data);
 
