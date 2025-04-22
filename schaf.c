b00142f6ce70ecd81f19e65cd54e6d4d470d7a8f
Tadashi Saito <tad.a.digger@gmail.com>
2025-04-22 10:59:48 +0900

    First try for union value.

--- schaf.c
+++ schaf.c
@@ -257,18 +257,19 @@ inline Value value_of_symbol(const char *s)
     return (Value) (sym << FLAG_NBIT_SYM | FLAG_SYM);
 }
 
-void *obj_new(size_t size, ValueTag t)
+SchObject *obj_new(ValueTag t)
 {
-    void *p = gc_malloc(size);
+    SchObject *p = gc_malloc(sizeof(SchObject));
     VALUE_TAG(p) = t;
     return p;
 }
 
 Value value_of_string(const char *s)
 {
-    String *str = obj_new(sizeof(String), TAG_STRING);
+    SchObject *o = obj_new(TAG_STRING);
+    String *str = STRING(o);
     str->body = xstrdup(s);
-    return (Value) str;
+    return (Value) o;
 }
 
 static void expect_cfunc_arity(int64_t actual)
@@ -315,7 +316,8 @@ static Value apply_cfunc_3(Value env, CFunc *f, Value args)
 static Value value_of_cfunc(const char *name, void *cfunc, int64_t arity)
 {
     expect_cfunc_arity(arity);
-    CFunc *f = obj_new(sizeof(CFunc), TAG_CFUNC);
+    SchObject *o = obj_new(TAG_CFUNC);
+    CFunc *f = &o->cfunc;
     f->name = xstrdup(name);
     f->proc.arity = arity;
     f->cfunc = cfunc;
@@ -338,7 +340,7 @@ static Value value_of_cfunc(const char *name, void *cfunc, int64_t arity)
     default:
         error("invalid arity: %"PRId64, arity);
     }
-    return (Value) f;
+    return (Value) o;
 }
 
 static Value value_of_syntax(const char *name, void *cfunc, int64_t arity)
@@ -350,12 +352,14 @@ static Value value_of_syntax(const char *name, void *cfunc, int64_t arity)
 
 static Value value_of_closure(Value env, Value params, Value body)
 {
-    Closure *f = obj_new(sizeof(Closure), TAG_CLOSURE);
-    f->proc.arity = (params == Qnil || value_is_pair(params)) ? length(params) : -1;
+    SchObject *o = obj_new(TAG_CLOSURE);
+    bool headp = (params == Qnil || value_is_pair(params));
+    Closure *f = &o->closure;
+    f->proc.arity = headp ? length(params) : -1;
     f->env = env;
     f->params = params;
     f->body = body;
-    return (Value) f;
+    return (Value) o;
 }
 
 // and `cons` is well-known name than "value_of_pair"
@@ -389,9 +393,10 @@ static Value runtime_error(const char *fmt, ...)
     vsnprintf(errmsg, sizeof(errmsg), fmt, ap);
     va_end(ap);
 
-    Error *e = obj_new(sizeof(Error), TAG_ERROR);
+    SchObject *o = obj_new(TAG_ERROR);
+    Error *e = &o->error;
     e->call_stack = Qnil;
-    return (Value) e;
+    return (Value) o;
 }
 
 const char *error_message(void)
@@ -492,10 +497,11 @@ static Value append2(Value l1, Value l2)
 
 static Value env_new(void)
 {
-    Env *e = obj_new(sizeof(Env), TAG_ENV);
+    SchObject *o = obj_new(TAG_ENV);
+    Env *e = ENV(o);
     e->table = table_new();
     e->parent = Qfalse;
-    return (Value) e;
+    return (Value) o;
 }
 
 static Value env_inherit(Value parent)
@@ -1625,10 +1631,10 @@ static Value proc_pair_p(UNUSED Value env, Value o)
 
 Value cons(Value car, Value cdr)
 {
-    Pair *p = obj_new(sizeof(Pair), TAG_PAIR);
-    p->car = car;
-    p->cdr = cdr;
-    return (Value) p;
+    SchObject *o = obj_new(TAG_PAIR);
+    o->pair.car = car;
+    o->pair.cdr = cdr;
+    return (Value) o;
 }
 
 inline Value car(Value v)
@@ -1940,12 +1946,13 @@ static Value proc_for_each(Value env, Value args)
 
 static Value value_of_continuation(void)
 {
-    Continuation *c = obj_new(sizeof(Continuation), TAG_CONTINUATION);
+    SchObject *o = obj_new(TAG_CONTINUATION);
+    Continuation *c = &o->continuation;
     c->proc.arity = 1; // by spec
     c->sp = c->stack = NULL;
     c->stack_len = 0;
     c->retval = Qfalse;
-    return (Value) c;
+    return (Value) o;
 }
 
 [[gnu::noinline]]
