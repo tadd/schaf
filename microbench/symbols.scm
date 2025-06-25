bd0a2edcaa2843cd368a10ee158ecdf7f2a88a48
Tadashi Saito <tad.a.digger@gmail.com>
2025-06-25 22:16:28 +0900

    Add generated test code to stablize benchmarking.

---
+++ microbench/symbols.scm
@@ -0,0 +1,1359 @@
+(define vaa 'vaa)
+(define vab 'vab)
+(define vac 'vac)
+(define vad 'vad)
+(define vae 'vae)
+(define vaf 'vaf)
+(define vag 'vag)
+(define vah 'vah)
+(define vai 'vai)
+(define vaj 'vaj)
+(define vak 'vak)
+(define val 'val)
+(define vam 'vam)
+(define van 'van)
+(define vao 'vao)
+(define vap 'vap)
+(define vaq 'vaq)
+(define var 'var)
+(define vas 'vas)
+(define vat 'vat)
+(define vau 'vau)
+(define vav 'vav)
+(define vaw 'vaw)
+(define vax 'vax)
+(define vay 'vay)
+(define vaz 'vaz)
+(define vba 'vba)
+(define vbb 'vbb)
+(define vbc 'vbc)
+(define vbd 'vbd)
+(define vbe 'vbe)
+(define vbf 'vbf)
+(define vbg 'vbg)
+(define vbh 'vbh)
+(define vbi 'vbi)
+(define vbj 'vbj)
+(define vbk 'vbk)
+(define vbl 'vbl)
+(define vbm 'vbm)
+(define vbn 'vbn)
+(define vbo 'vbo)
+(define vbp 'vbp)
+(define vbq 'vbq)
+(define vbr 'vbr)
+(define vbs 'vbs)
+(define vbt 'vbt)
+(define vbu 'vbu)
+(define vbv 'vbv)
+(define vbw 'vbw)
+(define vbx 'vbx)
+(define vby 'vby)
+(define vbz 'vbz)
+(define vca 'vca)
+(define vcb 'vcb)
+(define vcc 'vcc)
+(define vcd 'vcd)
+(define vce 'vce)
+(define vcf 'vcf)
+(define vcg 'vcg)
+(define vch 'vch)
+(define vci 'vci)
+(define vcj 'vcj)
+(define vck 'vck)
+(define vcl 'vcl)
+(define vcm 'vcm)
+(define vcn 'vcn)
+(define vco 'vco)
+(define vcp 'vcp)
+(define vcq 'vcq)
+(define vcr 'vcr)
+(define vcs 'vcs)
+(define vct 'vct)
+(define vcu 'vcu)
+(define vcv 'vcv)
+(define vcw 'vcw)
+(define vcx 'vcx)
+(define vcy 'vcy)
+(define vcz 'vcz)
+(define vda 'vda)
+(define vdb 'vdb)
+(define vdc 'vdc)
+(define vdd 'vdd)
+(define vde 'vde)
+(define vdf 'vdf)
+(define vdg 'vdg)
+(define vdh 'vdh)
+(define vdi 'vdi)
+(define vdj 'vdj)
+(define vdk 'vdk)
+(define vdl 'vdl)
+(define vdm 'vdm)
+(define vdn 'vdn)
+(define vdo 'vdo)
+(define vdp 'vdp)
+(define vdq 'vdq)
+(define vdr 'vdr)
+(define vds 'vds)
+(define vdt 'vdt)
+(define vdu 'vdu)
+(define vdv 'vdv)
+(define vdw 'vdw)
+(define vdx 'vdx)
+(define vdy 'vdy)
+(define vdz 'vdz)
+(define vea 'vea)
+(define veb 'veb)
+(define vec 'vec)
+(define ved 'ved)
+(define vee 'vee)
+(define vef 'vef)
+(define veg 'veg)
+(define veh 'veh)
+(define vei 'vei)
+(define vej 'vej)
+(define vek 'vek)
+(define vel 'vel)
+(define vem 'vem)
+(define ven 'ven)
+(define veo 'veo)
+(define vep 'vep)
+(define veq 'veq)
+(define ver 'ver)
+(define ves 'ves)
+(define vet 'vet)
+(define veu 'veu)
+(define vev 'vev)
+(define vew 'vew)
+(define vex 'vex)
+(define vey 'vey)
+(define vez 'vez)
+(define vfa 'vfa)
+(define vfb 'vfb)
+(define vfc 'vfc)
+(define vfd 'vfd)
+(define vfe 'vfe)
+(define vff 'vff)
+(define vfg 'vfg)
+(define vfh 'vfh)
+(define vfi 'vfi)
+(define vfj 'vfj)
+(define vfk 'vfk)
+(define vfl 'vfl)
+(define vfm 'vfm)
+(define vfn 'vfn)
+(define vfo 'vfo)
+(define vfp 'vfp)
+(define vfq 'vfq)
+(define vfr 'vfr)
+(define vfs 'vfs)
+(define vft 'vft)
+(define vfu 'vfu)
+(define vfv 'vfv)
+(define vfw 'vfw)
+(define vfx 'vfx)
+(define vfy 'vfy)
+(define vfz 'vfz)
+(define vga 'vga)
+(define vgb 'vgb)
+(define vgc 'vgc)
+(define vgd 'vgd)
+(define vge 'vge)
+(define vgf 'vgf)
+(define vgg 'vgg)
+(define vgh 'vgh)
+(define vgi 'vgi)
+(define vgj 'vgj)
+(define vgk 'vgk)
+(define vgl 'vgl)
+(define vgm 'vgm)
+(define vgn 'vgn)
+(define vgo 'vgo)
+(define vgp 'vgp)
+(define vgq 'vgq)
+(define vgr 'vgr)
+(define vgs 'vgs)
+(define vgt 'vgt)
+(define vgu 'vgu)
+(define vgv 'vgv)
+(define vgw 'vgw)
+(define vgx 'vgx)
+(define vgy 'vgy)
+(define vgz 'vgz)
+(define vha 'vha)
+(define vhb 'vhb)
+(define vhc 'vhc)
+(define vhd 'vhd)
+(define vhe 'vhe)
+(define vhf 'vhf)
+(define vhg 'vhg)
+(define vhh 'vhh)
+(define vhi 'vhi)
+(define vhj 'vhj)
+(define vhk 'vhk)
+(define vhl 'vhl)
+(define vhm 'vhm)
+(define vhn 'vhn)
+(define vho 'vho)
+(define vhp 'vhp)
+(define vhq 'vhq)
+(define vhr 'vhr)
+(define vhs 'vhs)
+(define vht 'vht)
+(define vhu 'vhu)
+(define vhv 'vhv)
+(define vhw 'vhw)
+(define vhx 'vhx)
+(define vhy 'vhy)
+(define vhz 'vhz)
+(define via 'via)
+(define vib 'vib)
+(define vic 'vic)
+(define vid 'vid)
+(define vie 'vie)
+(define vif 'vif)
+(define vig 'vig)
+(define vih 'vih)
+(define vii 'vii)
+(define vij 'vij)
+(define vik 'vik)
+(define vil 'vil)
+(define vim 'vim)
+(define vin 'vin)
+(define vio 'vio)
+(define vip 'vip)
+(define viq 'viq)
+(define vir 'vir)
+(define vis 'vis)
+(define vit 'vit)
+(define viu 'viu)
+(define viv 'viv)
+(define viw 'viw)
+(define vix 'vix)
+(define viy 'viy)
+(define viz 'viz)
+(define vja 'vja)
+(define vjb 'vjb)
+(define vjc 'vjc)
+(define vjd 'vjd)
+(define vje 'vje)
+(define vjf 'vjf)
+(define vjg 'vjg)
+(define vjh 'vjh)
+(define vji 'vji)
+(define vjj 'vjj)
+(define vjk 'vjk)
+(define vjl 'vjl)
+(define vjm 'vjm)
+(define vjn 'vjn)
+(define vjo 'vjo)
+(define vjp 'vjp)
+(define vjq 'vjq)
+(define vjr 'vjr)
+(define vjs 'vjs)
+(define vjt 'vjt)
+(define vju 'vju)
+(define vjv 'vjv)
+(define vjw 'vjw)
+(define vjx 'vjx)
+(define vjy 'vjy)
+(define vjz 'vjz)
+(define vka 'vka)
+(define vkb 'vkb)
+(define vkc 'vkc)
+(define vkd 'vkd)
+(define vke 'vke)
+(define vkf 'vkf)
+(define vkg 'vkg)
+(define vkh 'vkh)
+(define vki 'vki)
+(define vkj 'vkj)
+(define vkk 'vkk)
+(define vkl 'vkl)
+(define vkm 'vkm)
+(define vkn 'vkn)
+(define vko 'vko)
+(define vkp 'vkp)
+(define vkq 'vkq)
+(define vkr 'vkr)
+(define vks 'vks)
+(define vkt 'vkt)
+(define vku 'vku)
+(define vkv 'vkv)
+(define vkw 'vkw)
+(define vkx 'vkx)
+(define vky 'vky)
+(define vkz 'vkz)
+(define vla 'vla)
+(define vlb 'vlb)
+(define vlc 'vlc)
+(define vld 'vld)
+(define vle 'vle)
+(define vlf 'vlf)
+(define vlg 'vlg)
+(define vlh 'vlh)
+(define vli 'vli)
+(define vlj 'vlj)
+(define vlk 'vlk)
+(define vll 'vll)
+(define vlm 'vlm)
+(define vln 'vln)
+(define vlo 'vlo)
+(define vlp 'vlp)
+(define vlq 'vlq)
+(define vlr 'vlr)
+(define vls 'vls)
+(define vlt 'vlt)
+(define vlu 'vlu)
+(define vlv 'vlv)
+(define vlw 'vlw)
+(define vlx 'vlx)
+(define vly 'vly)
+(define vlz 'vlz)
+(define vma 'vma)
+(define vmb 'vmb)
+(define vmc 'vmc)
+(define vmd 'vmd)
+(define vme 'vme)
+(define vmf 'vmf)
+(define vmg 'vmg)
+(define vmh 'vmh)
+(define vmi 'vmi)
+(define vmj 'vmj)
+(define vmk 'vmk)
+(define vml 'vml)
+(define vmm 'vmm)
+(define vmn 'vmn)
+(define vmo 'vmo)
+(define vmp 'vmp)
+(define vmq 'vmq)
+(define vmr 'vmr)
+(define vms 'vms)
+(define vmt 'vmt)
+(define vmu 'vmu)
+(define vmv 'vmv)
+(define vmw 'vmw)
+(define vmx 'vmx)
+(define vmy 'vmy)
+(define vmz 'vmz)
+(define vna 'vna)
+(define vnb 'vnb)
+(define vnc 'vnc)
+(define vnd 'vnd)
+(define vne 'vne)
+(define vnf 'vnf)
+(define vng 'vng)
+(define vnh 'vnh)
+(define vni 'vni)
+(define vnj 'vnj)
+(define vnk 'vnk)
+(define vnl 'vnl)
+(define vnm 'vnm)
+(define vnn 'vnn)
+(define vno 'vno)
+(define vnp 'vnp)
+(define vnq 'vnq)
+(define vnr 'vnr)
+(define vns 'vns)
+(define vnt 'vnt)
+(define vnu 'vnu)
+(define vnv 'vnv)
+(define vnw 'vnw)
+(define vnx 'vnx)
+(define vny 'vny)
+(define vnz 'vnz)
+(define voa 'voa)
+(define vob 'vob)
+(define voc 'voc)
+(define vod 'vod)
+(define voe 'voe)
+(define vof 'vof)
+(define vog 'vog)
+(define voh 'voh)
+(define voi 'voi)
+(define voj 'voj)
+(define vok 'vok)
+(define vol 'vol)
+(define vom 'vom)
+(define von 'von)
+(define voo 'voo)
+(define vop 'vop)
+(define voq 'voq)
+(define vor 'vor)
+(define vos 'vos)
+(define vot 'vot)
+(define vou 'vou)
+(define vov 'vov)
+(define vow 'vow)
+(define vox 'vox)
+(define voy 'voy)
+(define voz 'voz)
+(define vpa 'vpa)
+(define vpb 'vpb)
+(define vpc 'vpc)
+(define vpd 'vpd)
+(define vpe 'vpe)
+(define vpf 'vpf)
+(define vpg 'vpg)
+(define vph 'vph)
+(define vpi 'vpi)
+(define vpj 'vpj)
+(define vpk 'vpk)
+(define vpl 'vpl)
+(define vpm 'vpm)
+(define vpn 'vpn)
+(define vpo 'vpo)
+(define vpp 'vpp)
+(define vpq 'vpq)
+(define vpr 'vpr)
+(define vps 'vps)
+(define vpt 'vpt)
+(define vpu 'vpu)
+(define vpv 'vpv)
+(define vpw 'vpw)
+(define vpx 'vpx)
+(define vpy 'vpy)
+(define vpz 'vpz)
+(define vqa 'vqa)
+(define vqb 'vqb)
+(define vqc 'vqc)
+(define vqd 'vqd)
+(define vqe 'vqe)
+(define vqf 'vqf)
+(define vqg 'vqg)
+(define vqh 'vqh)
+(define vqi 'vqi)
+(define vqj 'vqj)
+(define vqk 'vqk)
+(define vql 'vql)
+(define vqm 'vqm)
+(define vqn 'vqn)
+(define vqo 'vqo)
+(define vqp 'vqp)
+(define vqq 'vqq)
+(define vqr 'vqr)
+(define vqs 'vqs)
+(define vqt 'vqt)
+(define vqu 'vqu)
+(define vqv 'vqv)
+(define vqw 'vqw)
+(define vqx 'vqx)
+(define vqy 'vqy)
+(define vqz 'vqz)
+(define vra 'vra)
+(define vrb 'vrb)
+(define vrc 'vrc)
+(define vrd 'vrd)
+(define vre 'vre)
+(define vrf 'vrf)
+(define vrg 'vrg)
+(define vrh 'vrh)
+(define vri 'vri)
+(define vrj 'vrj)
+(define vrk 'vrk)
+(define vrl 'vrl)
+(define vrm 'vrm)
+(define vrn 'vrn)
+(define vro 'vro)
+(define vrp 'vrp)
+(define vrq 'vrq)
+(define vrr 'vrr)
+(define vrs 'vrs)
+(define vrt 'vrt)
+(define vru 'vru)
+(define vrv 'vrv)
+(define vrw 'vrw)
+(define vrx 'vrx)
+(define vry 'vry)
+(define vrz 'vrz)
+(define vsa 'vsa)
+(define vsb 'vsb)
+(define vsc 'vsc)
+(define vsd 'vsd)
+(define vse 'vse)
+(define vsf 'vsf)
+(define vsg 'vsg)
+(define vsh 'vsh)
+(define vsi 'vsi)
+(define vsj 'vsj)
+(define vsk 'vsk)
+(define vsl 'vsl)
+(define vsm 'vsm)
+(define vsn 'vsn)
+(define vso 'vso)
+(define vsp 'vsp)
+(define vsq 'vsq)
+(define vsr 'vsr)
+(define vss 'vss)
+(define vst 'vst)
+(define vsu 'vsu)
+(define vsv 'vsv)
+(define vsw 'vsw)
+(define vsx 'vsx)
+(define vsy 'vsy)
+(define vsz 'vsz)
+(define vta 'vta)
+(define vtb 'vtb)
+(define vtc 'vtc)
+(define vtd 'vtd)
+(define vte 'vte)
+(define vtf 'vtf)
+(define vtg 'vtg)
+(define vth 'vth)
+(define vti 'vti)
+(define vtj 'vtj)
+(define vtk 'vtk)
+(define vtl 'vtl)
+(define vtm 'vtm)
+(define vtn 'vtn)
+(define vto 'vto)
+(define vtp 'vtp)
+(define vtq 'vtq)
+(define vtr 'vtr)
+(define vts 'vts)
+(define vtt 'vtt)
+(define vtu 'vtu)
+(define vtv 'vtv)
+(define vtw 'vtw)
+(define vtx 'vtx)
+(define vty 'vty)
+(define vtz 'vtz)
+(define vua 'vua)
+(define vub 'vub)
+(define vuc 'vuc)
+(define vud 'vud)
+(define vue 'vue)
+(define vuf 'vuf)
+(define vug 'vug)
+(define vuh 'vuh)
+(define vui 'vui)
+(define vuj 'vuj)
+(define vuk 'vuk)
+(define vul 'vul)
+(define vum 'vum)
+(define vun 'vun)
+(define vuo 'vuo)
+(define vup 'vup)
+(define vuq 'vuq)
+(define vur 'vur)
+(define vus 'vus)
+(define vut 'vut)
+(define vuu 'vuu)
+(define vuv 'vuv)
+(define vuw 'vuw)
+(define vux 'vux)
+(define vuy 'vuy)
+(define vuz 'vuz)
+(define vva 'vva)
+(define vvb 'vvb)
+(define vvc 'vvc)
+(define vvd 'vvd)
+(define vve 'vve)
+(define vvf 'vvf)
+(define vvg 'vvg)
+(define vvh 'vvh)
+(define vvi 'vvi)
+(define vvj 'vvj)
+(define vvk 'vvk)
+(define vvl 'vvl)
+(define vvm 'vvm)
+(define vvn 'vvn)
+(define vvo 'vvo)
+(define vvp 'vvp)
+(define vvq 'vvq)
+(define vvr 'vvr)
+(define vvs 'vvs)
+(define vvt 'vvt)
+(define vvu 'vvu)
+(define vvv 'vvv)
+(define vvw 'vvw)
+(define vvx 'vvx)
+(define vvy 'vvy)
+(define vvz 'vvz)
+(define vwa 'vwa)
+(define vwb 'vwb)
+(define vwc 'vwc)
+(define vwd 'vwd)
+(define vwe 'vwe)
+(define vwf 'vwf)
+(define vwg 'vwg)
+(define vwh 'vwh)
+(define vwi 'vwi)
+(define vwj 'vwj)
+(define vwk 'vwk)
+(define vwl 'vwl)
+(define vwm 'vwm)
+(define vwn 'vwn)
+(define vwo 'vwo)
+(define vwp 'vwp)
+(define vwq 'vwq)
+(define vwr 'vwr)
+(define vws 'vws)
+(define vwt 'vwt)
+(define vwu 'vwu)
+(define vwv 'vwv)
+(define vww 'vww)
+(define vwx 'vwx)
+(define vwy 'vwy)
+(define vwz 'vwz)
+(define vxa 'vxa)
+(define vxb 'vxb)
+(define vxc 'vxc)
+(define vxd 'vxd)
+(define vxe 'vxe)
+(define vxf 'vxf)
+(define vxg 'vxg)
+(define vxh 'vxh)
+(define vxi 'vxi)
+(define vxj 'vxj)
+(define vxk 'vxk)
+(define vxl 'vxl)
+(define vxm 'vxm)
+(define vxn 'vxn)
+(define vxo 'vxo)
+(define vxp 'vxp)
+(define vxq 'vxq)
+(define vxr 'vxr)
+(define vxs 'vxs)
+(define vxt 'vxt)
+(define vxu 'vxu)
+(define vxv 'vxv)
+(define vxw 'vxw)
+(define vxx 'vxx)
+(define vxy 'vxy)
+(define vxz 'vxz)
+(define vya 'vya)
+(define vyb 'vyb)
+(define vyc 'vyc)
+(define vyd 'vyd)
+(define vye 'vye)
+(define vyf 'vyf)
+(define vyg 'vyg)
+(define vyh 'vyh)
+(define vyi 'vyi)
+(define vyj 'vyj)
+(define vyk 'vyk)
+(define vyl 'vyl)
+(define vym 'vym)
+(define vyn 'vyn)
+(define vyo 'vyo)
+(define vyp 'vyp)
+(define vyq 'vyq)
+(define vyr 'vyr)
+(define vys 'vys)
+(define vyt 'vyt)
+(define vyu 'vyu)
+(define vyv 'vyv)
+(define vyw 'vyw)
+(define vyx 'vyx)
+(define vyy 'vyy)
+(define vyz 'vyz)
+(define vza 'vza)
+(define vzb 'vzb)
+(define vzc 'vzc)
+(define vzd 'vzd)
+(define vze 'vze)
+(define vzf 'vzf)
+(define vzg 'vzg)
+(define vzh 'vzh)
+(define vzi 'vzi)
+(define vzj 'vzj)
+(define vzk 'vzk)
+(define vzl 'vzl)
+(define vzm 'vzm)
+(define vzn 'vzn)
+(define vzo 'vzo)
+(define vzp 'vzp)
+(define vzq 'vzq)
+(define vzr 'vzr)
+(define vzs 'vzs)
+(define vzt 'vzt)
+(define vzu 'vzu)
+(define vzv 'vzv)
+(define vzw 'vzw)
+(define vzx 'vzx)
+(define vzy 'vzy)
+(define vzz 'vzz)
+
+(define (syms)
+  vjd
+  vsg
+  vqy
+  vas
+  vvw
+  vda
+  vtw
+  vpe
+  vpg
+  vtx
+  vik
+  vhk
+  vwm
+  vtm
+  vaj
+  vqw
+  vsh
+  vvc
+  vfc
+  vxu
+  vgo
+  vot
+  vzq
+  vyt
+  vqo
+  vnr
+  vmy
+  vrb
+  vja
+  vbx
+  vfb
+  vfe
+  vlr
+  vuj
+  vwt
+  vqa
+  vyw
+  vhi
+  vyc
+  vhg
+  vgx
+  vmk
+  vfz
+  vap
+  vgb
+  vxn
+  vao
+  voi
+  vwp
+  vwd
+  vpl
+  vxi
+  vqp
+  vim
+  vnl
+  vss
+  vvk
+  vpy
+  vdh
+  vfk
+  vex
+  vyn
+  vla
+  vdq
+  vih
+  vfp
+  vpi
+  vls
+  vrv
+  vob
+  vzf
+  vnf
+  vdr
+  voh
+  vfg
+  vkq
+  vun
+  vqh
+  vee
+  vre
+  vuu
+  vka
+  vvd
+  vlm
+  vrf
+  vht
+  vkv
+  vgj
+  vhu
+  vaq
+  vyf
+  vkd
+  vip
+  vsn
+  vge
+  ven
+  veg
+  ved
+  vwk
+  vln
+  vga
+  vte
+  vqe
+  vif
+  vpq
+  vyh
+  vcf
+  vxs
+  ver
+  vhy
+  von
+  vxo
+  vta
+  vrh
+  vry
+  vrn
+  vaf
+  vcr
+  vep
+  vxq
+  vtl
+  vuh
+  val
+  vfu
+  vca
+  vng
+  vxc
+  vnp
+  vsd
+  vsx
+  vjj
+  vpm
+  vkg
+  vye
+  vdg
+  vbz
+  vri
+  vvp
+  vxt
+  vuo
+  viq
+  vku
+  vum
+  vhs
+  vqf
+  vgf
+  vce
+  vne
+  vdu
+  vpx
+  vay
+  vqt
+  vza
+  vxm
+  vok
+  vbk
+  vba
+  vcp
+  vqs
+  vxz
+  vse
+  vmz
+  vvt
+  veh
+  vac
+  vtj
+  vbb
+  vna
+  vur
+  vqd
+  vin
+  vpv
+  vwl
+  vju
+  vyj
+  vab
+  vnc
+  voj
+  vnx
+  vbi
+  vui
+  vjk
+  vkf
+  vrj
+  vil
+  vly
+  vsq
+  vtg
+  vpo
+  vud
+  vaz
+  vuw
+  vrx
+  vkp
+  vts
+  vlb
+  vbh
+  vey
+  vkl
+  vpb
+  vhr
+  vvi
+  vrw
+  vir
+  vhw
+  vhb
+  vtu
+  vyx
+  vft
+  vyd
+  vwu
+  vci
+  vrz
+  vrs
+  vpa
+  vhh
+  vag
+  vgk
+  vkw
+  vvz
+  vhq
+  vjn
+  vnv
+  vvv
+  vvy
+  vbp
+  vbc
+  vvs
+  vjs
+  vhv
+  vjz
+  viy
+  vmt
+  vcy
+  vat
+  vlh
+  vfw
+  vxf
+  vdi
+  vfx
+  vwa
+  vah
+  vxw
+  vdk
+  vig
+  vke
+  vbv
+  vsr
+  vnb
+  vbe
+  vio
+  vxd
+  vwv
+  vwh
+  vyg
+  vho
+  vpd
+  vsj
+  vzs
+  vwi
+  vld
+  vix
+  vfr
+  vew
+  vdf
+  vrp
+  vqx
+  vrl
+  vze
+  vmu
+  vav
+  vwj
+  vqu
+  vzb
+  vak
+  vvx
+  vqb
+  vje
+  vem
+  vdt
+  vlv
+  vug
+  vcg
+  vmb
+  vox
+  vul
+  vog
+  vbw
+  vrr
+  vkc
+  vnd
+  vtd
+  vle
+  vod
+  vnh
+  vky
+  vub
+  vqi
+  vji
+  voc
+  vty
+  vxj
+  vux
+  vjg
+  vns
+  vyu
+  voo
+  vhm
+  vet
+  vdy
+  vgw
+  vsv
+  vwq
+  vgt
+  vbo
+  vds
+  vym
+  vlg
+  vov
+  vfv
+  vgz
+  vad
+  vsa
+  vou
+  vck
+  vfh
+  vdo
+  vru
+  vyz
+  vmd
+  vgn
+  vut
+  vfa
+  vlz
+  vwn
+  vma
+  vtq
+  van
+  vhp
+  vpn
+  vsu
+  vlj
+  vzg
+  vjb
+  vhl
+  vii
+  vqm
+  vrk
+  vcx
+  vtt
+  vbq
+  vwe
+  vhx
+  vvb
+  vyb
+  vto
+  vdc
+  vrt
+  vqv
+  vff
+  vvo
+  vlw
+  vez
+  vsf
+  vhn
+  vjy
+  vau
+  vxx
+  vvn
+  vnt
+  vtb
+  vcd
+  vqn
+  vpz
+  vgs
+  vmf
+  vwr
+  vxh
+  vql
+  vsl
+  vlo
+  vnm
+  vuv
+  vis
+  vel
+  vqk
+  vcz
+  vid
+  viv
+  vlt
+  vwz
+  vkr
+  vzm
+  vpu
+  vzn
+  vch
+  vmc
+  vmv
+  vbl
+  vam
+  vxy
+  vzh
+  vms
+  vvq
+  vwb
+  vfm
+  vxk
+  vop
+  vgg
+  vgl
+  vuc
+  vkb
+  viu
+  vwc
+  vgp
+  vij
+  vnw
+  vzr
+  vdl
+  vcj
+  vxa
+  voz
+  vys
+  vhj
+  vjp
+  vni
+  vme
+  vpt
+  vyv
+  vjr
+  vdp
+  vml
+  vzu
+  vdn
+  vsc
+  vek
+  vib
+  vzx
+  vmg
+  vdb
+  vol
+  vcm
+  vsi
+  vai
+  vtv
+  vxr
+  vow
+  vdx
+  vzv
+  vkk
+  vmx
+  vsm
+  vvj
+  vcn
+  vkt
+  vrq
+  vcu
+  vbf
+  vyl
+  vrm
+  vxe
+  vdz
+  vgu
+  vjo
+  vhc
+  vmp
+  var
+  vjm
+  vmo
+  vti
+  vya
+  vpr
+  vom
+  vtk
+  vjc
+  vzj
+  vra
+  vnq
+  vva
+  vmw
+  veq
+  vtr
+  vby
+  voa
+  vkx
+  vvl
+  vbs
+  vxp
+  vcl
+  vjw
+  vyy
+  vbg
+  vqg
+  vzl
+  vfi
+  voe
+  vsy
+  vwo
+  vpj
+  vjv
+  vtn
+  vsb
+  vki
+  vth
+  vgc
+  viz
+  vrg
+  vso
+  vec
+  vae
+  vbn
+  vor
+  vzd
+  vgq
+  vll
+  vdd
+  vnn
+  vno
+  vvh
+  vzy
+  vsk
+  vco
+  vhe
+  vyq
+  vyr
+  vhz
+  vmi
+  vzc
+  vjf
+  vdv
+  vqj
+  vfs
+  vus
+  vwx
+  vfo
+  vie
+  vmm
+  vqr
+  vzp
+  vlq
+  vbj
+  vsp
+  vzi
+  vmn
+  vww
+  vuf
+  vmh
+  vpp
+  vyp
+  vuk
+  vqz
+  vgv
+  vha
+  vbd
+  veb
+  vyk
+  vfj
+  vnu
+  vny
+  vtp
+  voy
+  vcq
+  vde
+  vxb
+  vgd
+  vst
+  vvm
+  vvu
+  vqc
+  vic
+  vmq
+  vcs
+  vyo
+  vzt
+  vfd
+  vlp
+  vcw
+  vvg
+  vev
+  vlk
+  vct
+  vcb
+  vof
+  vve
+  vcv
+  vjh
+  vrc
+  vuq
+  vgy
+  vtf
+  vcc
+  vei
+  vvf
+  vua
+  vzz
+  vdm
+  vlc
+  veo
+  vbt
+  vpf
+  vsw
+  vfq
+  vdw
+  vnj
+  vlf
+  vyi
+  vrd
+  vbu
+  ves
+  vjt
+  vfy
+  vit
+  vkh
+  vue
+  vaa
+  vej
+  vqq
+  vvr
+  vsz
+  vos
+  vpw
+  voq
+  vks
+  vfl
+  vjl
+  vxg
+  vpc
+  vea
+  vmr
+  vnz
+  via
+  vfn
+  vxl
+  viw
+  vnk
+  vbr
+  vzk
+  vgm
+  vwf
+  vkj
+  vhf
+  vws
+  vjq
+  vkz
+  vuy
+  vdj
+  veu
+  vxv
+  vuz
+  vhd
+  vzw
+  vjx
+  vtz
+  vlx
+  vko
+  vgi
+  vlu
+  vwg
+  vgr
+  vph
+  vef
+  vkm
+  vmj
+  vwy
+  vro
+  vpk
+  vaw
+  vbm
+  vps
+  vkn
+  vzo
+  vli
+  vup
+  vgh
+  vtc
+  vax
+)
+
+(do ((i 0 (+ i 1)))
+    ((> i 60000) i)
+  (syms))
