315ad352bb5dfb90a36e6bfe80d8f7e2cf3dc068
Tadashi Saito <tad.a.digger@gmail.com>
2025-07-08 11:13:19 +0900

    Compact union objects.

--- intern.h
+++ intern.h
@@ -61,10 +61,6 @@ typedef struct {
 } LocatedPair;
 
 typedef struct {
-    char *body;
-} String;
-
-typedef struct {
     int64_t arity;
     Value (*apply)(Value env, Value proc, Value args);
 } Procedure;
@@ -89,17 +85,17 @@ typedef struct {
 } Closure;
 
 typedef struct {
-    Procedure proc;
     uintptr_t *sp;
     void *stack;
     size_t stack_len;
-    jmp_buf state;
-    Value retval;
-} Continuation;
+    jmp_buf regs;
+} ExecutionState;
 
 typedef struct {
-    Value *body;// use scary
-} Vector;
+    Procedure proc;
+    Value retval;
+    ExecutionState *exstate;
+} Continuation;
 
 typedef struct {
     char *name;
@@ -120,23 +116,19 @@ typedef struct {
 } StackFrame;
 
 typedef struct {
-    StackFrame **call_stack;
-} Error;
-
-typedef struct {
     Header header; // common
     union {
-        String string;
+        char *string;
         Pair pair;
         LocatedPair lpair;
         Procedure proc;
         CFunc cfunc;
         Closure closure;
         Continuation continuation;
-        Vector vector;
+        Value *vector;// use scary
         Env env;
         Port port;
-        Error error;
+        StackFrame **error;// ditto
     };
 } SchObject;
 
@@ -146,16 +138,16 @@ typedef struct {
 
 #define PAIR(v) (&OBJ(v)->pair)
 #define LOCATED_PAIR(v) (&OBJ(v)->lpair)
-#define STRING(v) (&OBJ(v)->string)
+#define STRING(v) (OBJ(v)->string)
 #define PROCEDURE(v) (&OBJ(v)->proc)
 #define CFUNC(v) (&OBJ(v)->cfunc)
 #define CLOSURE(v) (&OBJ(v)->closure)
 #define CONTINUATION(v) (&OBJ(v)->continuation)
-#define VECTOR(v) (&OBJ(v)->vector)
+#define VECTOR(v) (OBJ(v)->vector)
 #define PARSER(v) (&OBJ(v)->parser)
 #define ENV(v) (&OBJ(v)->env)
 #define PORT(v) (&OBJ(v)->port)
-#define ERROR(v) (&OBJ(v)->error)
+#define ERROR(v) (OBJ(v)->error)
 
 typedef struct {
     char *filename;
