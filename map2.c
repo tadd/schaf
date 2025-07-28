4b01be8934f73a466e0ab233213f26b91e59c231
Tadashi Saito <tad.a.digger@gmail.com>
2025-07-28 21:17:00 +0900

    Trying 3-args with ret (ret + 2 arguments)

--- map2.c
+++ map2.c
@@ -43,10 +43,18 @@
 #define MAPARG_1(c, a, x, ...) c(a, x) __VA_OPT__(, MAPARG_2(c, a, __VA_ARGS__))
 #define MAPARG(c, a, ...)              __VA_OPT__(MAPARG_1(c, a, __VA_ARGS__))
 
+#define MAPARG_TWIN_3(c, a, x, y, ...) c(a, x, y), c(a, y, x), c(a, y, y) \
+        __VA_OPT__(, MAPARG_TWIN_4(c, a, y, __VA_ARGS__))
+#define MAPARG_TWIN_2(c, a, x, y, ...) c(a, x, y), c(a, y, x), c(a, y, y) \
+        __VA_OPT__(, MAPARG_TWIN_3(c, a, y, __VA_ARGS__))
+#define MAPARG_TWIN_1(c, a, x, ...) c(a, x, x) \
+        __VA_OPT__(, MAPARG_TWIN_2(c, a, x, __VA_ARGS__))
+#define MAPARG_TWIN(c, a, ...)  __VA_OPT__(MAPARG_TWIN_1(c, a, __VA_ARGS__))
+
 #define UNWRAP(...) __VA_ARGS__
 
 #define MAP2_TWIN_6(c, ys, x, ...) MAPARG(c, x, UNWRAP ys) \
-        __VA_OPT__(, MAP2_TWIN_7(c, ys, __VA_ARGS__))
+        __VA_OPT__("Too many arguments!")
 #define MAP2_TWIN_5(c, ys, x, ...) MAPARG(c, x, UNWRAP ys) \
         __VA_OPT__(, MAP2_TWIN_6(c, ys, __VA_ARGS__))
 #define MAP2_TWIN_4(c, ys, x, ...) MAPARG(c, x, UNWRAP ys) \
@@ -58,14 +66,27 @@
 #define MAP2_TWIN_1(c, ys, ...) __VA_OPT__(MAP2_TWIN_2(c, ys, __VA_ARGS__))
 #define MAP2_TWIN(c, xs, ys) MAP2_TWIN_1(c, ys, UNWRAP xs)
 
+#define MAP3_TWIN_6(c, ys, x, ...) MAPARG_TWIN(c, x, UNWRAP ys) \
+        __VA_OPT__("Too many arguments!")
+#define MAP3_TWIN_5(c, ys, x, ...) MAPARG_TWIN(c, x, UNWRAP ys) \
+        __VA_OPT__(, MAP3_TWIN_6(c, ys, __VA_ARGS__))
+#define MAP3_TWIN_4(c, ys, x, ...) MAPARG_TWIN(c, x, UNWRAP ys) \
+        __VA_OPT__(, MAP3_TWIN_5(c, ys, __VA_ARGS__))
+#define MAP3_TWIN_3(c, ys, x, ...) MAPARG_TWIN(c, x, UNWRAP ys) \
+        __VA_OPT__(, MAP3_TWIN_4(c, ys, __VA_ARGS__))
+#define MAP3_TWIN_2(c, ys, x, ...) MAPARG_TWIN(c, x, UNWRAP ys) \
+        __VA_OPT__(, MAP3_TWIN_3(c, ys, __VA_ARGS__))
+#define MAP3_TWIN_1(c, ys, ...) __VA_OPT__(MAP3_TWIN_2(c, ys, __VA_ARGS__))
+#define MAP3_TWIN(c, xs, ys) MAP3_TWIN_1(c, ys, UNWRAP xs)
+
 #define TYPES_ARG Value, int64_t, const char*
 #define TYPES_RET TYPES_ARG, void, bool
 #define TY3_PAT(t, u, v) Value (*)(Value, t, u, v): 3
-#define TY2_PAT(t, u) Value (*)(Value, t, u): 2
+#define TY2_PAT(t, u, v) t (*)(Value, u, v): 2
 #define TY1_PAT(t, u) t (*)(Value, u): 1
 #define TY0_PAT(t) t (*)(Value): 0
 #define TY3 MAP3(TY3_PAT, TYPES_ARG)
-#define TY2 MAP2(TY2_PAT, TYPES_ARG)
+#define TY2 MAP3_TWIN(TY2_PAT, (TYPES_RET), (TYPES_ARG))
 #define TY1 MAP2_TWIN(TY1_PAT, (TYPES_RET), (TYPES_ARG))
 #define TY0 MAP(TY0_PAT, TYPES_RET)
 #define PATS TY0, TY1, TY2, TY3
@@ -74,7 +95,7 @@
 Value f(Value env);
 Value g(Value env, const char *s);
 Value h(Value env, int64_t i);
-Value i(Value env, int64_t i, const char *s);
+bool i(Value env, int64_t i, const char *s);
 Value j(Value env, int64_t i, const char *s, Value v);
 bool k(Value env);
 void l(Value env);
