398ddf3ba80e3ed451a6b2d11de17a2e27e01f29
Tadashi Saito <tad.a.digger@gmail.com>
2025-08-09 11:36:56 +0900

    Align to Object, be larger.

--- gc.c
+++ gc.c
@@ -83,7 +83,8 @@ void sch_set_gc_strategy(SchGCStrategy s)
 
 static inline size_t align(size_t size)
 {
-    return (size + 15U) / 16U * 16U;
+    const size_t n = sizeof(SchObject);
+    return (size + (n-1)) / n * n;
 }
 
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
@@ -130,7 +131,7 @@ void gc_init(uintptr_t *volatile sp)
 static Header *allocate_from_chunk(Header *prev, Header *curr, size_t size)
 {
     Header *next = HEADER_NEXT(curr);
-    if (curr->size > size + sizeof(Header)) {
+    if (curr->size > size) {
         Header *rest = (Header *)((uint8_t *) curr + size);
         init_chunk(rest, curr->size - size);
         HEADER_NEXT(rest) = next;
@@ -204,8 +205,7 @@ static bool is_valid_pointer(Value v)
 static bool is_valid_header(Value v)
 {
     Header *h = HEADER(v);
-    return is_valid_tag(h->tag) &&
-        h->size >= sizeof(SchObject) && h->size < sizeof(SchObject) * 2;
+    return is_valid_tag(h->tag) && h->size == sizeof(SchObject);
 }
 
 static bool in_heap_val(Value v)
