tadd/my-c-lisp

Try rapid hash.

basic-test.c
@@ -242,5 +242,7 @@ Test(table, get_put) {
     for (int i = 1; i <= 17; i++)
         cr_assert(eq(int, i*10000000, int_table_get(t, i)));
 
+    void int_table_dump(const IntTable *t);
+    int_table_dump(t);
     int_table_free(t);
 }

tadd/schaf

test/basic-test.c
@@ -349,6 +349,8 @@ Test(table, get_put) {
     for (int i = 1; i <= 17; i++)
         cr_assert(eq(llong, i*10000000, table_get(t, i)));
 
+    void table_dump(const Table *t);
+    table_dump(t);
     table_free(t);
 }
 