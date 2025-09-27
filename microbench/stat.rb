0137a81574b67f98ae6d94490dbb5e64b15f56f7
Tadashi Saito <tad.a.digger@gmail.com>
2025-09-28 02:28:28 +0900

    Dump stdev of time too.

--- microbench/stat.rb
+++ microbench/stat.rb
@@ -1,12 +1,17 @@
 require 'enumerable/statistics'
 
-def mean(vals, label, n = 0)
+def values(vals, label, n = 0)
   vals.grep(/#{label}/) do |l|
     l.split[1].to_f
-  end.mean.round(n)
+  end
 end
 
+def percent(val, base) = (100 * val / base).round(2)
+
 l = ARGF.readlines
-cpu = mean(l, "CPU", 3)
-mem = mean(l, "Vm")
-puts "%.3f\t%d" % [cpu, mem]
+cpu = values(l, "CPU")
+mem = values(l, "Vm")
+mcpu, dcpu = [cpu.mean, cpu.stdev].map{ _1.round(3) }
+pdcpu = percent(dcpu, mcpu)
+mmem = mem.mean.round
+puts "%.3f\t%.2f\t%d" % [mcpu, pdcpu, mmem]
