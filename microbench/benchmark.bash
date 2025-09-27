d83ce3631c2a04d694baf27fbb975ed7f7c9b72f
Tadashi Saito <tad.a.digger@gmail.com>
2025-09-28 02:11:56 +0900

    Dump TSV with "make microbench OUT=output.tsv" support.

--- microbench/benchmark.bash
+++ microbench/benchmark.bash
@@ -4,21 +4,29 @@ set -o pipefail
 nloop=10
 benchflags=-TM
 
+label() {
+    printf 'Test\tTime (msec)\tMemory (KiB)\n'
+}
+
 run() {
     local -r bin="$1"
     sudo echo -n
+    label
     for t in *.scm; do
-        printf '%12s\t' $t
+        printf '%s\t' ${t/.scm/}
 	benchmark-run -n $nloop ../$bin $benchflags $t 2>&1 | ruby stat.rb
     done
 }
 
 main() {
-    local bin=schaf
+    local bin=schaf out=/dev/null
     if (( $# >= 1 )); then
         bin="$1"
     fi
-    run "$bin"
+    if (( $# >= 2 )) && [[ ! -z "$2" ]]; then
+        out="$2"
+    fi
+    run "$bin" | tee "$out"
 }
 
 main "$@"
