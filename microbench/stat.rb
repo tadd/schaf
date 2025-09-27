320ae7549ca8d5972ab255e87804b1023af654c2
Tadashi Saito <tad.a.digger@gmail.com>
2025-09-28 01:45:58 +0900

    Refactor benchmarking scripts.

---
+++ microbench/stat.rb
@@ -0,0 +1,12 @@
+require 'enumerable/statistics'
+
+def mean(vals, label, n = 0)
+  vals.grep(/#{label}/) do |l|
+    l.split[1].to_f
+  end.mean.round(n)
+end
+
+l = ARGF.readlines
+cpu = mean(l, "CPU", 3)
+mem = mean(l, "Vm")
+puts "%.3f ms\t%6d kb" % [cpu, mem]
