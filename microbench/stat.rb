2e107093d87a0eb9624f24a5b77c48731999d1eb
Tadashi Saito <tad.a.digger@gmail.com>
2025-09-28 02:48:27 +0900

    Refactoring: Move more logic to Ruby.

--- microbench/stat.rb
+++ microbench/stat.rb
@@ -1,17 +1,30 @@
 require 'enumerable/statistics'
 
-def values(vals, label, n = 0)
-  vals.grep(/#{label}/) do |l|
-    l.split[1].to_f
-  end
-end
+def get_data = STDIN.gets.split[1].to_f
 
 def percent(val, base) = (100 * val / base).round(2)
 
-l = ARGF.readlines
-cpu = values(l, "CPU")
-mem = values(l, "Vm")
-mcpu, dcpu = [cpu.mean, cpu.stdev].map{ _1.round(3) }
-pdcpu = percent(dcpu, mcpu)
-mmem = mem.mean.round
-puts "%.3f\t%.2f\t%d" % [mcpu, pdcpu, mmem]
+def stat(prog, n)
+  cpu = []
+  mem = []
+  print "#{prog.chomp}\t"
+  STDOUT.flush
+  n.times do
+    cpu << get_data
+    mem << get_data
+  end
+  mcpu, dcpu = [cpu.mean, cpu.stdev].map{ _1.round(3) }
+  pdcpu = percent(dcpu, mcpu)
+  mmem = mem.mean.round
+  puts "%.3f\t%.2f\t%d" % [mcpu, pdcpu, mmem]
+  STDOUT.flush
+end
+
+def main(n)
+  puts "Test\tTime (msec)\tstdev (%)\tMemory (KiB)"
+  while (prog = STDIN.gets)
+    stat(prog.chomp, n)
+  end
+end
+
+main(ARGV[0].to_i)
