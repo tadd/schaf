d00da9881cc418e92b142cd29f72490d1cfff4bc
Tadashi Saito <tad.a.digger@gmail.com>
2025-09-28 01:45:58 +0900

    Refactor benchmarking scripts.

--- microbench/benchmark.bash
+++ microbench/benchmark.bash
@@ -6,15 +6,10 @@ benchflags=-TM
 
 run() {
     local -r bin="$1"
+    sudo echo -n
     for t in *.scm; do
-        printf '%12s	' $t
-	benchmark-run -n $nloop ../$bin $benchflags $t 2>&1|
-            ruby -renumerable/statistics -e '
-def mean(vals, label, n=0) = vals.grep(/#{label}/) { _1.split[1].to_f }.mean.round(n)
-l = ARGF.readlines
-cpu = mean(l, "CPU", 3)
-mem = mean(l, "Vm")
-puts "%.3f ms\t%6d kb" % [cpu, mem]'
+        printf '%12s\t' $t
+	benchmark-run -n $nloop ../$bin $benchflags $t 2>&1 | ruby stat.rb
     done
 }
 
