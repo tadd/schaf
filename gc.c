82cc21fa1bafec28ed8e1fd5b4110d8593f70ee9
Tadashi Saito <tad.a.digger@gmail.com>
2025-11-02 22:48:08 +0900

    Refactor with visitor pattern.

--- gc.c
+++ gc.c
@@ -162,27 +162,6 @@ static bool is_heap_value(Value v)
     return is_valid_pointer(v) && in_heap_range(v) && is_valid_header(v);
 }
 
-static void mark_val(Value v);
-
-static void mark_array(const void *volatile beg, size_t n)
-{
-    UNPOISON(beg, n * sizeof(uintptr_t));
-    const Value *volatile p = beg;
-    for (size_t i = 0; i < n; i++, p++)
-        mark_val(*p);
-}
-
-static void mark_jmpbuf(const jmp_buf *jmp)
-{
-    mark_array(jmp, idivceil(sizeof(jmp_buf), sizeof(uintptr_t)));
-}
-
-static void mark_env_each(UNUSED uint64_t key, uint64_t value, UNUSED void *data)
-{
-    // key is always a Symbol
-    mark_val(value);
-}
-
 static uintptr_t bitmap_index(const void *p);
 
 static bool is_living(MSHeader *h, bool do_mark)
@@ -205,54 +184,71 @@ static bool is_living(MSHeader *h, bool do_mark)
     return marked;
 }
 
-static void mark_val(Value v)
+typedef void (*ValueVisitor)(Value v);
+
+static void visit_array(ValueVisitor visit, const void *volatile beg, size_t n)
 {
-    if (!is_heap_value(v))
-        return;
-    MSHeader *h = MS_HEADER_FROM_VAL(v);
-    if (is_living(h, true)) // mark it!
-        return;
+    UNPOISON(beg, n * sizeof(uintptr_t));
+    const Value *volatile p = beg;
+    for (size_t i = 0; i < n; i++, p++)
+        visit(*p);
+}
+
+static void visit_jmpbuf(ValueVisitor visit, const jmp_buf *jmp)
+{
+    visit_array(visit, jmp, idivceil(sizeof(jmp_buf), sizeof(uintptr_t)));
+}
+
+static void visit_env_each(UNUSED uint64_t key, uint64_t value, void *p)
+{
+    ValueVisitor visit = p;
+    // key is always a Symbol
+    visit(value);
+}
+
+static void visit_children(Value v, ValueVisitor visit)
+{
     switch (VALUE_TAG(v)) {
     case TAG_PAIR: {
         Pair *p = PAIR(v);
-        mark_val(p->car);
-        mark_val(p->cdr);
+        visit(p->car);
+        visit(p->cdr);
         break;
     }
     case TAG_CLOSURE: {
         Closure *p = CLOSURE(v);
-        mark_val(p->env);
-        mark_val(p->params);
-        mark_val(p->body);
+        visit(p->env);
+        visit(p->params);
+        visit(p->body);
         break;
     }
     case TAG_CONTINUATION: {
         Continuation *p = CONTINUATION(v);
-        mark_val(p->retval);
-        mark_jmpbuf(&p->state);
-        mark_array(p->stack, idivceil(p->stack_len, sizeof(uintptr_t)));
+        visit(p->retval);
+        visit_jmpbuf(visit, &p->state);
+        visit_array(visit, p->stack, idivceil(p->stack_len, sizeof(uintptr_t)));
         break;
     }
     case TAG_CFUNC_CLOSURE:
-        mark_val(CFUNC_CLOSURE(v)->data);
+        visit(CFUNC_CLOSURE(v)->data);
         break;
     case TAG_ENV: {
         Env *p = ENV(v);
         if (p->table != NULL)
-            table_foreach(p->table, mark_env_each, NULL);
-        mark_val(p->parent);
+            table_foreach(p->table, visit_env_each, visit);
+        visit(p->parent);
         break;
     }
     case TAG_VECTOR: {
         Value *p = VECTOR(v);
         for (int64_t i = 0, len = scary_length(p); i < len; i++)
-            mark_val(p[i]);
+            visit(p[i]);
         break;
     }
     case TAG_PROMISE: {
         Promise *p = PROMISE(v);
-        mark_val(p->env);
-        mark_val(p->val);
+        visit(p->env);
+        visit(p->val);
         break;
     }
     case TAG_STRING:
@@ -263,6 +259,13 @@ static void mark_val(Value v)
     case TAG_ERROR:
         break;
     }
+}
+
+static void mark_val(Value v)
+{
+    // mark with `true`
+    if (is_heap_value(v) && !is_living(MS_HEADER_FROM_VAL(v), true))
+        visit_children(v, mark_val);
 }
 
 static void *allocate_from_chunk(MSHeap *heap, MSHeader *prev, MSHeader *curr, size_t size)
@@ -422,11 +425,16 @@ static void mark_roots(Value **roots)
         mark_val(*roots[i]);
 }
 
+static void mark_jmpbuf(const jmp_buf *jmp)
+{
+    visit_array(mark_val, jmp, idivceil(sizeof(jmp_buf), sizeof(uintptr_t)));
+}
+
 [[gnu::noinline]]
 static void mark_stack(void)
 {
     GET_SP(sp);
-    mark_array(sp, stack_base - sp);
+    visit_array(mark_val, sp, stack_base - sp);
 }
 
 static void mark(MSHeap *heap)
