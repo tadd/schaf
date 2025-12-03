81e7feb6773f68a5d4b3ab087d964eba30e50c86
Tadashi Saito <tad.a.digger@gmail.com>
2025-12-03 20:02:15 +0900

    Refactoring: Simplify.
    Unfortunately we have implicit global state now.

--- schaf.c
+++ schaf.c
@@ -942,12 +942,11 @@ static void dump_stack_trace(StackFrame *const *call_stack)
     dump_frame(call_stack[len-1], NULL);
 }
 
-static void add_source(Source ***psource_data, const Source *newsrc)
+static void add_source(const Source *newsrc)
 {
-    scary_push((void ***) psource_data, (const void *) newsrc);
-    Source **data = *psource_data;
-    size_t len = scary_length(data);
-    gc_add_root(&data[len-1]->ast);
+    scary_push((void ***) &source_data, (const void *) newsrc);
+    size_t len = scary_length(source_data);
+    gc_add_root(&source_data[len-1]->ast);
 }
 
 static Value iload(FILE *in, const char *filename)
@@ -955,7 +954,7 @@ static Value iload(FILE *in, const char *filename)
     Source *src = iparse(in, filename);
     if (src == NULL)
         return Qundef;
-    add_source(&source_data, src);
+    add_source(src);
     if (setjmp(jmp_exit) != 0)
         return sch_integer_new(exit_status);
     Value ret = eval_body(env_toplevel, src->ast);
@@ -971,7 +970,7 @@ static Value iload_inner(FILE *in, const char *path)
     Source *src = iparse(in, path);
     if (src == NULL)
         return Qundef;
-    add_source(&source_data, src);
+    add_source(src);
     return eval_body(env_toplevel, src->ast);
 }
 
