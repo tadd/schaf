f0c75b726ced661d1500685af4942747a6ff5f47
Tadashi Saito <tad.a.digger@gmail.com>
2026-02-03 02:33:11 +0900

    WIP

--- microbench/benchmark.bash
+++ microbench/benchmark.bash
@@ -4,16 +4,18 @@ set -o pipefail
 benchflags=-TM
 
 run() {
-    local -r bin="$1"
+    local -r bin="$1" nloop="$2"
     for t in *.scm; do
         printf '%12s	' $t
-	benchmark-run -n $nloop ../$bin $benchflags $t 2>&1|
-            ruby -renumerable/statistics -e '
-def mean(vals, label, n=0) = vals.grep(/#{label}/) { _1.split[1].to_f }.mean.round(n)
-l = ARGF.readlines
-cpu = mean(l, "CPU", 3)
-mem = mean(l, "Vm")
-puts "%.3f ms\t%6d kb" % [cpu, mem]'
+	echo benchmark-run -n $nloop ../$bin $benchflags $t 2>&1
+	# benchmark-run -n $nloop ../$bin $benchflags $t 2>&1
+# |
+#             ruby -renumerable/statistics -e '
+# def mean(vals, label, n=0) = vals.grep(/#{label}/) { _1.split[1].to_f }.mean.round(n)
+# l = ARGF.readlines
+# cpu = mean(l, "CPU", 3)
+# mem = mean(l, "Vm")
+# puts "%.3f ms\t%6d kb" % [cpu, mem]'
     done
 }
 
